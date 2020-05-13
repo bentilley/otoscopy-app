a05034037d008194c88b6a42d49602f2ae6862a6
bentilley <tilleyben@hotmail.com>
2020-05-13 16:21:09 +0100

    Colours - create global colours varaible

---
+++ components/design/colours.tsx
@@ -0,0 +1,27 @@
+/** @format */
+
+interface COLOURS {
+  black: string;
+  veryDark: string;
+  dark: string;
+  lessDark: string;
+  grey: string;
+  lightGrey: string;
+  primary: string;
+  pirmaryLight: string;
+  primaryDark: string;
+  favourite: string;
+}
+
+export const COLOURS: COLOURS = Object.freeze({
+  black: '#010101',
+  veryDark: '#131313',
+  dark: '#2f2f2f',
+  lessDark: '#3e3e3e',
+  grey: '#bdbdbd',
+  lightGrey: '#dedede',
+  primary: '#15d6be',
+  pirmaryLight: '#68fff0',
+  primaryDark: '#00a48e',
+  favourite: '#cfb113',
+});
