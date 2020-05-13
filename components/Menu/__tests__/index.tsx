2593ec88416935036feb86c85e9d1bfd6f6821e0
bentilley <tilleyben@hotmail.com>
2020-05-13 10:49:53 +0100

    Menu Component - write simple render and navigation tests

--- components/Menu/__tests__/index.tsx
+++ components/Menu/__tests__/index.tsx
@@ -1,12 +1,47 @@
 /** @format */
 
 import React from 'react';
-import { Menu } from '../index';
-import { render } from '@testing-library/react-native';
+import Menu from '../index';
+import { render, fireEvent } from '@testing-library/react-native';
+
+let navigationStubs: {
+  goToReference: () => void;
+  goToFavourites: () => void;
+  goToBrowse: () => void;
+};
+
+beforeEach(() => {
+  navigationStubs = {
+    goToReference: jest.fn(),
+    goToFavourites: jest.fn(),
+    goToBrowse: jest.fn(),
+  };
+});
 
 describe('<Menu />', () => {
   it('renders correctly', () => {
-    const { getByText } = render(<Menu />);
-    expect(getByText('Hello')).toBeTruthy();
+    const { queryByText } = render(<Menu {...navigationStubs} />);
+    expect(queryByText('Menu')).toBeTruthy();
+  });
+
+  it('navigates to the Reference menu', () => {
+    const { getByText } = render(<Menu {...navigationStubs} />);
+    const btn = getByText('Conditions');
+    fireEvent.press(btn);
+    expect(navigationStubs.goToReference).toHaveBeenCalled();
+  });
+
+  it('navigates to the Favourites menu', () => {
+    const { getByText } = render(<Menu {...navigationStubs} />);
+    const btn = getByText('Favourites');
+    fireEvent.press(btn);
+    expect(navigationStubs.goToFavourites).toHaveBeenCalled();
+  });
+
+  it('navigates to the Slide screen on random browse', () => {
+    const { getByText } = render(<Menu {...navigationStubs} />);
+    const btn = getByText('Random Browse');
+    fireEvent.press(btn);
+    expect(navigationStubs.goToBrowse).toHaveBeenCalled();
   });
 });
