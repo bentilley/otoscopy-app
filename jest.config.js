9deca20ec2c46d845a5b3f95efb8a6888cf4c22f
bentilley <tilleyben@hotmail.com>
2020-05-18 12:49:58 +0100

    Jest - update test config for react-native-firebase
    This needs to be whitelisted for compilation, and mocked.
    
    I think the mock doesn't actually get used, as I realised I also had to
    mock the hook that was using firebase. But I've left this in in case
    mocking firebase is useful for future tests.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
+/** @format */
+
 module.exports = {
   verbose: true,
   // preset: 'react-native',
   preset: '@testing-library/react-native',
+  modulePaths: ['<rootDir>'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-navigation|react-navigation-redux-helpers|@react-navigation/.*)',
+    'node_modules/(?!' +
+      '(jest-)?react-native' +
+      '|react-navigation' +
+      '|react-navigation-redux-helpers' +
+      '|@react-navigation/.*' +
+      '|@react-native-firebase/.*' +
+      ')',
   ],
 };
