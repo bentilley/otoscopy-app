e0c2b435e8683b41f1b5634b9f1c9f309a5b9e19
bentilley <tilleyben@hotmail.com>
2020-05-20 18:11:45 +0100

    useCondition - create useCondition hook to handle getting condition data
    The hook checks whether the data is in memory before getting it from
    Firebase.

--- model/condition/index.tsx
+++ model/condition/index.tsx
@@ -11,7 +11,10 @@ async function getCategories(
   setCategories(categories as Category[]);
 }
 
-const initialState = { categories: [] };
+const initialState = {
+  categories: [],
+  conditions: {},
+};
 
 const ConditionStateContext = React.createContext<State>(initialState);
 
@@ -25,19 +28,51 @@ export const ConditionProvider: React.FC<Props> = ({ children }) => {
     getCategories(setCategories);
   }, [setCategories]);
 
+  const [conditions, setConditions] = React.useState<ConditionDataState>({});
+
   return (
-    <ConditionStateContext.Provider value={{ categories }}>
+    <ConditionStateContext.Provider
+      value={{ categories, conditions, setConditions }}>
       {children}
     </ConditionStateContext.Provider>
   );
 };
 
 export const useConditions = () => {
-  const context = React.useContext(ConditionStateContext);
+  const context = React.useContext<State>(ConditionStateContext);
   if (context === undefined) {
     throw new Error('Probably not in Provider!');
   }
-  return context;
+  const { categories, conditions } = context;
+  return { categories, conditions };
+};
+
+export const useCondition = (conditionId: string) => {
+  const { conditions, setConditions } = React.useContext(ConditionStateContext);
+  if (!setConditions) {
+    throw new TypeError('setConditions is not defined here!');
+  }
+
+  if (conditions[conditionId]) {
+    return { info: conditions[conditionId] };
+  } else {
+    const newConditions = { ...conditions };
+    firestore()
+      .collection('conditions')
+      .doc(conditionId)
+      .get()
+      .then(
+        (doc) => {
+          const data = doc.data();
+          newConditions[conditionId] = data as ConditionData;
+          setConditions(newConditions);
+        },
+        (err) => {
+          console.error(err);
+        },
+      );
+    return { info: null };
+  }
 };
 
 export type Category = {
@@ -50,6 +85,32 @@ export type Condition = {
   name: string;
 };
 
+export type ConditionData = {
+  name: string;
+  description: string;
+  aetiology: ConditionSection;
+  audiology: ConditionSection;
+  clinical_signs: ConditionSection;
+  complications: ConditionSection;
+  investigations: ConditionSection;
+  management: ConditionSection;
+  otoscopy: ConditionSection;
+  population: ConditionSection;
+  risk_factors: ConditionSection;
+  symptoms: ConditionSection;
+};
+
+export type ConditionSection = string[] | { [index: string]: SectionDetail };
+
+type SectionDetail = {
+  title: string;
+  information: string[];
+};
+
+type ConditionDataState = { [index: string]: ConditionData };
+
 type State = {
   categories: Category[];
+  conditions: ConditionDataState;
+  setConditions?: React.Dispatch<React.SetStateAction<ConditionDataState>>;
 };
