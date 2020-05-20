f6599944d73c72cdba368ebd560cee69ddd7e1c5
bentilley <tilleyben@hotmail.com>
2020-05-20 18:07:26 +0100

    App Tests - fix failing menu test since button change

--- __tests__/App.tsx
+++ __tests__/App.tsx
@@ -11,7 +11,7 @@ describe('<App />', () => {
   it('renders correctly on the menu screen', () => {
     const { getByText, getAllByText } = render(<App />);
 
-    expect(getAllByText('Menu')).toBeTruthy();
+    expect(getAllByText('Otoscopy App')).toBeTruthy();
     expect(getByText('Conditions')).toBeTruthy();
     expect(getByText('Favourites')).toBeTruthy();
     expect(getByText('Random Browse')).toBeTruthy();
