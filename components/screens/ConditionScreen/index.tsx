0068d9929ce913156c4d9a14b9276e8be4b1668b
bentilley <tilleyben@hotmail.com>
2020-05-20 18:13:08 +0100

    ConditionView - create ConditionView component [wip]
    This is pretty much there. There are a few aesthetic tweaks that need to
    be made regarding font weight and bullet positioning.

--- components/screens/ConditionScreen/index.tsx
+++ components/screens/ConditionScreen/index.tsx
@@ -1,11 +1,11 @@
 /** @format */
 
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import { RootStackParamList } from 'App';
+import ConditionView from 'components/ConditionView';
 
 type ConditionProps = {
   navigation: StackNavigationProp<RootStackParamList, 'Condition'>;
@@ -13,22 +13,17 @@ type ConditionProps = {
 };
 
 const ConditionScreen: React.FC<ConditionProps> = ({ route, navigation }) => {
+  const navigationFunctions = {
+    goToSlides: () => {
+      navigation.navigate('SlideList', { slides: 'condition slides' });
+    },
+  };
   return (
-    <View style={styles.screen}>
-      <Text>Condition</Text>
-      <Text>Here are some facts about {route.params.condition}</Text>
-      <Button
-        title="View Slides"
-        onPress={() =>
-          navigation.navigate('SlideList', { slides: 'condition slides' })
-        }
-      />
-    </View>
+    <ConditionView
+      {...navigationFunctions}
+      condition={route.params.condition}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  screen: { flex: 1, alignItems: 'center', justifyContent: 'center' },
-});
 
 export default ConditionScreen;
