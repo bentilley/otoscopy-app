f52b27be17ca812a023dbd5f8f2154f7952fb836
bentilley <tilleyben@hotmail.com>
2020-05-21 16:20:58 +0100

    Utils - create utils module and add hash function

---
+++ utils/hash.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+export function shortHash(str: string): number {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    let chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
---
+++ utils/index.ts
@@ -0,0 +1,3 @@
+/** @format */
+
+export { shortHash } from './hash';
