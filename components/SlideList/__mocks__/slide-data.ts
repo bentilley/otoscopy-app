3d1f2eed375060c5a3b945d83d0d8c35eb6ebce7
bentilley <tilleyben@hotmail.com>
2020-05-28 17:23:26 +0100

    SlideList - update slide list mock data with images

--- components/SlideList/__mocks__/slide-data.ts
+++ components/SlideList/__mocks__/slide-data.ts
@@ -3,7 +3,8 @@
 export const slideData = [
   {
     condition: 'Otitis Media',
-    diagnosis: 'Right ear Acute Otitis Media.',
+    diagnosis:
+      'Right ear Acute Otitis Media. Bulging tympanic membrane with purulent effusion visible.',
     diagram: '<diagram-url>',
     img_url: '/slide-img/otitis-media/100001.jpg',
     legend: {
@@ -12,7 +13,8 @@ export const slideData = [
       PT: 'Pars Tensa',
       U: 'Umbo',
     },
-    thumbnail_url: '/thumb/otitis-media/100001.jpg',
+    thumbnail_url:
+      'https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/800/cached.offlinehbpl.hbpl.co.uk/news/PGH/023A264D-00E4-AB9E-E53B57ADCFD6FF8C.jpg',
   },
   {
     condition: 'Otitis Media',
@@ -25,7 +27,8 @@ export const slideData = [
       PT: 'Pars Tensa',
       U: 'Umbo',
     },
-    thumbnail_url: '/thumb/otitis-media/100002.jpg',
+    thumbnail_url:
+      'https://upload.wikimedia.org/wikipedia/commons/5/58/Otitis_media_entdifferenziert2.jpg',
   },
   {
     condition: 'Otitis Media',
@@ -38,6 +41,6 @@ export const slideData = [
       PT: 'Pars Tensa',
       U: 'Umbo',
     },
-    thumbnail_url: '/thumb/otitis-media/100003.jpg',
+    thumbnail_url: 'https://img.medscapestatic.com/pi/meds/ckb/16/44916tn.jpg',
   },
 ];
