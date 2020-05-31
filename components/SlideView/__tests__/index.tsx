8ebeaa9a08d1fa7fe4b21c2c6c438544a78341fa
bentilley <tilleyben@hotmail.com>
2020-05-31 19:05:54 +0100

    SlideView - create test skeleton for SlideView component

---
+++ components/SlideView/__tests__/index.tsx
@@ -0,0 +1,40 @@
+/** @format */
+
+import React from 'react';
+import SlideView from '../index';
+import { render, fireEvent, cleanup } from '@testing-library/react-native';
+import { slideData } from 'components/SlideList/__mocks__/slide-data';
+import { Slide } from 'model/condition/types';
+
+// This is a fix for a TouchableOpacity bug - see
+// https://github.com/testing-library/native-testing-library/issues/113 to see
+// if there is a fix yet and this can be removed
+jest.mock(
+  'react-native/Libraries/Components/Touchable/TouchableOpacity',
+  () => 'TouchableOpacity',
+);
+
+let navigationStubs: {
+  goToCondition: () => void;
+};
+
+let props: { slide: Slide } = { slide: slideData[0] };
+
+beforeEach(() => {
+  navigationStubs = {
+    goToCondition: jest.fn(),
+  };
+
+  props.slide = slideData[0];
+});
+
+afterEach(cleanup);
+
+describe('<SlideList />', () => {
+  it('renders correctly', () => {
+    const { queryAllByText } = render(
+      <SlideView {...navigationStubs} {...props} />,
+    );
+    expect(queryAllByText('view slide').length).toEqual(3);
+  });
+});
