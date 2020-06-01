37a1909af5c432c04aaad721b57b6947a2341c3b
bentilley <tilleyben@hotmail.com>
2020-06-01 18:04:43 +0100

    SlideView - create simple panResponder to handle otoscope animation

--- components/SlideView/index.tsx
+++ components/SlideView/index.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Animated, PanResponder } from 'react-native';
 import { Slide } from 'model/condition/types';
 import { OtoText } from 'components/design';
 import { COLOURS } from 'components/design';
@@ -12,7 +12,38 @@ type Props = {
   goToCondition: () => void;
 };
 
+function currentDistFromCenter(x: number, y: number): number {
+  return Math.sqrt(x * x + y * y);
+}
+
 const SlideView: React.FC<Props> = ({ slide, goToCondition }) => {
+  const otoscopePos = React.useRef({ x: 0, y: 0 });
+  const otoscopePan = React.useRef(new Animated.ValueXY()).current;
+  const panResponder = React.useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        otoscopePan.setOffset({
+          x: otoscopePos.current.x,
+          y: otoscopePos.current.y,
+        });
+      },
+      onPanResponderMove: (_, gestureState) => {
+        if (
+          currentDistFromCenter(
+            otoscopePos.current.x + gestureState.dx,
+            otoscopePos.current.y + gestureState.dy,
+          ) < 100
+        ) {
+          otoscopePan.setValue({ x: gestureState.dx, y: gestureState.dy });
+        }
+      },
+      onPanResponderRelease: () => {
+        otoscopePan.flattenOffset();
+        otoscopePan.stopAnimation((pos) => (otoscopePos.current = pos));
+      },
+    }),
+  ).current;
   return (
     <React.Fragment>
       <View style={styles.screen}>
@@ -23,6 +54,19 @@ const SlideView: React.FC<Props> = ({ slide, goToCondition }) => {
         </View>
         <View style={styles.imageContainer}>
           <Image source={{ uri: slide.img_url }} style={styles.image} />
+          <Animated.View
+            style={[
+              otoscopeStyles.animation,
+              {
+                transform: [
+                  { translateX: otoscopePan.x },
+                  { translateY: otoscopePan.y },
+                ],
+              },
+            ]}
+            {...panResponder.panHandlers}>
+            <View style={otoscopeStyles.overlay} />
+          </Animated.View>
         </View>
         <View style={styles.spacer} />
       </View>
@@ -33,6 +77,20 @@ const SlideView: React.FC<Props> = ({ slide, goToCondition }) => {
   );
 };
 
+const otoscopeStyles = StyleSheet.create({
+  animation: {
+    position: 'absolute',
+    left: -200,
+  },
+  overlay: {
+    borderColor: COLOURS.dark,
+    borderWidth: 300,
+    width: 800,
+    height: 800,
+    borderRadius: 800 / 2,
+  },
+});
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
@@ -40,7 +98,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: COLOURS.black,
   },
-  imageContainer: { justifyContent: 'center' },
+  imageContainer: { justifyContent: 'center', overflow: 'hidden' },
   image: { width: 400, height: 400 },
   spacer: { flex: 1, justifyContent: 'center' },
 });
