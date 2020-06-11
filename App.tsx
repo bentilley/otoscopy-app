82fd2f8e9e03121be79518c0b5f73a398e318c53
bentilley <tilleyben@hotmail.com>
2020-06-11 17:51:20 +0100

    Sentry Error Logging - Set up Sentry for the app (android and ios)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 /** @format */
 
 import React from 'react';
+import * as Sentry from '@sentry/react-native';
 import { UserProvider } from 'model/user';
 import { LoginWrappedApp } from 'components';
+
+Sentry.init({
+  dsn:
+    'https://c3ce9e802b9e4b8f8c452289ca8c8312@o406224.ingest.sentry.io/5273264',
+});
 
 const App = () => {
   return (
