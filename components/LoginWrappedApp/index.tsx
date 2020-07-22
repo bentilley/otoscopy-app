46c091177c58715ed948792e079ff95d86911384
bentilley <tilleyben@hotmail.com>
2020-07-22 10:01:49 +0100

    TODOS, Deps - add todo for loading spinner, update babel/@core and react-native-vector-icons

--- components/LoginWrappedApp/index.tsx
+++ components/LoginWrappedApp/index.tsx
@@ -5,6 +5,15 @@ import { AppScreens } from 'components/screens';
 import { LoginScreens } from 'components/screens';
 import { useUser } from 'model/user';
 
+/**TODO Add loading spinner to login and sign up screen.
+
+# Description
+
+This should show on the transition to the app, once the user has signed in.
+
+@label UI
+*/
+
 export const LoginWrappedApp: React.FC = () => {
   const { user } = useUser();
   return user ? <AppScreens /> : <LoginScreens />;
