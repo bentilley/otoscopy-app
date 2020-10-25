992320ac7bff5bb7a4d46ccedff186c82d7e47de
bentilley <tilleyben@hotmail.com>
2020-10-25 09:35:01 +0000

    ConditionView - fix breaking tests

--- components/ConditionView/__tests__/index.tsx
+++ components/ConditionView/__tests__/index.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import ConditionView from '../index';
+import { Condition } from 'model/condition/types';
 import { render, fireEvent, cleanup } from '@testing-library/react-native';
 
 // This is a fix for a TouchableOpacity bug - see
@@ -46,17 +47,41 @@ let navigationStubs: {
   goToSlides: () => void;
 };
 
-let condition: {
-  id: string;
-  name: string;
-};
+let condition: Condition;
 
 beforeEach(() => {
   navigationStubs = {
     goToSlides: jest.fn(),
   };
 
-  condition = { name: 'Otitis Media', id: 'otitis' };
+  condition = {
+    id: 'otitis',
+    name: 'Otitis Media',
+    description: 'Acute onset inflamation of the middle ear space',
+    population: ['Commonly paediatric 3-7 years'],
+    aetiology: {
+      bacterial: {
+        information: [
+          'Strep Pneumoniae',
+          'Haemophilus',
+          'Moraxella Catarrhalis',
+        ],
+        title: 'Bacterial',
+      },
+      viral: {
+        information: ['Rhinovirus', 'RSV', 'Adenovirus'],
+        title: 'Viral (75% cases)',
+      },
+    },
+    audiology: {},
+    clinical_signs: {},
+    complications: {},
+    investigations: {},
+    management: {},
+    otoscopy: {},
+    risk_factors: {},
+    symptoms: {},
+  };
 });
 
 afterEach(cleanup);
@@ -72,7 +97,7 @@ describe('<ConditionView />', () => {
   });
 
   it('renders loading if there is no condition info', () => {
-    condition.id = 'null';
+    condition = null;
     const { queryByText } = render(
       <ConditionView {...navigationStubs} condition={condition} />,
     );
@@ -80,10 +105,10 @@ describe('<ConditionView />', () => {
   });
 
   it('navigates to the slide view when the slide button is pressed', () => {
-    const { getByTestId } = render(
+    const { getByText } = render(
       <ConditionView {...navigationStubs} condition={condition} />,
     );
-    const btn = getByTestId('view-slides-btn');
+    const btn = getByText('view slides');
     fireEvent.press(btn);
     expect(navigationStubs.goToSlides).toHaveBeenCalled();
   });
