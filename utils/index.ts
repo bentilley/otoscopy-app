320b3662a09a6ec403b444aa6e69915c44120dc6
bentilley <tilleyben@hotmail.com>
2021-03-23 23:35:32 +0000

    feat(utils): add a couple of new tools to utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,6 @@
 /** @format */
 
-export { shortHash } from './hash';
-export { checkIfInvalidContext, useSimpleUpdater } from './react';
-export { getRandomInt, clip } from './misc';
+export { shortHash } from "./hash";
+export { checkIfInvalidContext, useSimpleUpdater } from "./react";
+export { getRandomInt, clip, toStr } from "./misc";
+export { isList, isInfoLink } from "./type-predicates";
--- utils/misc.ts
+++ utils/misc.ts
@@ -1,4 +1,6 @@
 /** @format */
+import { InfoLink } from "model/condition/types";
+import { isInfoLink } from "./type-predicates";
 
 export function getRandomInt(max: number): number {
   return Math.floor(Math.random() * Math.floor(max));
@@ -6,8 +8,16 @@ export function getRandomInt(max: number): number {
 
 export function clip(str: string, clipLength: number): string {
   if (str.length > clipLength) {
-    return str.slice(0, clipLength) + '...';
+    return str.slice(0, clipLength) + "...";
   } else {
     return str;
+  }
+}
+
+export function toStr(s: string | InfoLink): string {
+  if (isInfoLink(s)) {
+    return s.text as string;
+  } else {
+    return s;
   }
 }
