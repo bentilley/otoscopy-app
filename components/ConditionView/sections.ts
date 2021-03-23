de060dfa455446e38ee30d69ceb6894f30e13b87
bentilley <tilleyben@hotmail.com>
2021-03-23 23:30:04 +0000

    feat(additional_resources): add new type to sections
    Also prettier quote reformatting on the file.

--- components/ConditionView/sections.ts
+++ components/ConditionView/sections.ts
@@ -1,41 +1,47 @@
 /** @format */
 
 type Sections =
-  | 'otoscopy'
-  | 'population'
-  | 'risk_factors'
-  | 'aetiology'
-  | 'symptoms'
-  | 'clinical_signs'
-  | 'investigations'
-  | 'audiology'
-  | 'management'
-  | 'complications';
+  | "otoscopy"
+  | "population"
+  | "risk_factors"
+  | "aetiology"
+  | "symptoms"
+  | "clinical_signs"
+  | "investigations"
+  | "audiology"
+  | "management"
+  | "complications"
+  | "additional_resources";
 
 export const sections: Sections[] = [
-  'otoscopy',
-  'population',
-  'risk_factors',
-  'aetiology',
-  'symptoms',
-  'clinical_signs',
-  'investigations',
-  'audiology',
-  'management',
-  'complications',
+  "otoscopy",
+  "population",
+  "risk_factors",
+  "aetiology",
+  "symptoms",
+  "clinical_signs",
+  "investigations",
+  "audiology",
+  "management",
+  "complications",
+  "additional_resources",
 ];
 
 export const sectionMeta: {
   [index: string]: { title: string; icon: string };
 } = {
-  otoscopy: { title: 'Otoscopy', icon: 'otoscope' },
-  population: { title: 'Population', icon: 'population' },
-  risk_factors: { title: 'Risk Factors', icon: 'risks' },
-  aetiology: { title: 'Aetiology', icon: 'causes' },
-  symptoms: { title: 'Symptoms', icon: 'symptoms' },
-  clinical_signs: { title: 'Clinical Signs', icon: 'stethoscope' },
-  investigations: { title: 'Investogations', icon: 'search' },
-  audiology: { title: 'Audiology', icon: 'ear' },
-  management: { title: 'Management', icon: 'treatment' },
-  complications: { title: 'Complications', icon: 'complications' },
+  otoscopy: { title: "Otoscopy", icon: "otoscope" },
+  population: { title: "Population", icon: "population" },
+  risk_factors: { title: "Risk Factors", icon: "risks" },
+  aetiology: { title: "Aetiology", icon: "causes" },
+  symptoms: { title: "Symptoms", icon: "symptoms" },
+  clinical_signs: { title: "Clinical Signs", icon: "stethoscope" },
+  investigations: { title: "Investigations", icon: "search" },
+  audiology: { title: "Audiology", icon: "ear" },
+  management: { title: "Management", icon: "treatment" },
+  complications: { title: "Complications", icon: "complications" },
+  additional_resources: {
+    title: "Additional Resources",
+    icon: "complications",
+  },
 };
