78220e536045897ea792291c87848bee53042d83
bentilley <tilleyben@hotmail.com>
2021-05-05 22:08:23 +0100

    fix(Sentry): don't load Sentry in development

--- services/error-handling/index.ts
+++ services/error-handling/index.ts
@@ -3,10 +3,12 @@
 import * as Sentry from "@sentry/react-native";
 
 export const setUpSentry = () => {
-  Sentry.init({
-    dsn:
-      "https://c3ce9e802b9e4b8f8c452289ca8c8312@o406224.ingest.sentry.io/5273264",
-  });
+  if (!__DEV__) {
+    Sentry.init({
+      dsn:
+        "https://c3ce9e802b9e4b8f8c452289ca8c8312@o406224.ingest.sentry.io/5273264",
+    });
+  }
   return Sentry;
 };
 
